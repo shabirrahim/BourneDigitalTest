756f17a5904a2969f49284f723b339804d936ee7
Gippy Singh <gippy@bournedigital.com.au>
2022-03-07 09:43:41 +0000

    initial commit

---
+++ sapui5-bourne-developer-test-cf/developertest/dist/controller/NotFound.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["au/com/bournedigital/developertest/controller/BaseController"],function(e){"use strict";return e.extend("au.com.bournedigital.developertest.controller.NotFound",{onLinkPressed:function(){this.getRouter().navTo("worklist")}})});
